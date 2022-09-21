ccb934b960700d6ad742f89d36d8c35304af50eb
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-09-20 23:00:00 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from './Post'
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Kristopher Mazzini"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia consequatur aspernatur ipsa impedit! Tenetur a sint, obcaecati culpa ratione quia dignissimos quod error molestiae veritatis, enim quas repellat nobis in!"
+      />
+      <Post
+        author="Maria Fernanda"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sequi excepturi accusamus, repellat et dignissimos quis vel. Molestias architecto, qui soluta ad suscipit excepturi quo debitis ipsa tenetur! Tenetur, modi!"
+      />
+    </div>
+  )
+}
